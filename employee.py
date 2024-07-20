bed161e93d403f3588bdfc9e0a5d7e63462e24a2
gurpreetsingh-5 <er.gur@outlook.com>
2024-07-20 11:17:39 +0530

    2nd day

---
+++ employee.py
@@ -0,0 +1,55 @@
+# import manager
+# print(manager.rohit_id)
+# print(manager.rohit_pass)
+
+# print(manager.employee_list)
+
+
+# print(manager.employee_list)
+
+# #callig-----
+# manager.rohit_hii()
+
+
+# import manager as mng # as means allias------
+
+# print(mng.rohit_id)
+# print(mng.rohit_pass)
+
+# print(mng.employee_list)
+
+
+
+# import manager as mng
+
+# print(mng.manager_name)
+# print(mng.manager_id)
+# print(mng.manager_pass)
+
+from manager import rohit_id
+from manager import rohit_pass
+from manager import rohit_hii
+# from manager import manager_id
+
+print(rohit_id)
+print(rohit_pass)
+print()
+rohit_hii()
+
+# print(manager_id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
